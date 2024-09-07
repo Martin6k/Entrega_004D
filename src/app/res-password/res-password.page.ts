dbc264faadc40001897c25265e0db1b541253bd1
IMarcuSI <166658868+IMarcuSI@users.noreply.github.com>
2024-09-07 19:22:20 -0400

    css

--- src/app/res-password/res-password.page.ts
+++ src/app/res-password/res-password.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-res-password',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResPasswordPage implements OnInit {
 
-  constructor() { }
+  constructor(private navCtrl: NavController) {
+   }
+   navigateToLogin() {
+    this.navCtrl.navigateForward('/home', {
+    });
+  }
 
   ngOnInit() {
+    
   }
 
 }
