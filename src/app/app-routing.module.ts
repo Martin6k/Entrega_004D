56583023707213f803b1e272331b6add10384c86
ma.gutierrezk <ma.gutierrezk@duocuc.cl>
2024-09-27 10:49:00 -0300

    authservice

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -30,6 +30,14 @@ const routes: Routes = [
     path: 'not-found',
     loadChildren: () => import('./not-found/not-found.module').then( m => m.NotFoundPageModule)
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./dashboard/dashboard.module').then( m => m.DashboardPageModule)
+  },
 
 
 
